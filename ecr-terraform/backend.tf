0ae4cab996fd1996e21d664340613127b0eb4bd1
abeddoura-prog <abed.doura@univ-constantine2.dz>
2026-01-08 13:33:38 +0100

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "aluruarumullaa1"
-    key    = "ecr/terraform.tfstate"
+    bucket = "doura-eks-bucket1-c8b5c833"   # ✅ استبدلنا بالـ bucket الذي أنشأته بنفسك
+    key    = "ecr/terraform.tfstate"        # ✅ أبقينا نفس المسار حتى لا يتغير هيكل المشروع
     region = "us-east-1"
   }
 
-  required_version = ">= 1.6.3"
+  required_version = ">= 1.6.0"
 }
